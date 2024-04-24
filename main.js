6c29f0f08a13440fb329e813125f01fe588e39e5
Ame-x <127585375+ame-x@users.noreply.github.com>
2024-04-25 02:11:27 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-
 $(function () {
     $('#btn').click(function () {
         var link = $('#link').val();
         var username = $('#username').val();
         var content = $('#content').val();
         var avatar = $('#avatar').val();
-        var delay = $('#delay').val();
         if (link == null || link == "", content == null || content == "") {
             alert("Webhook URL is empty");
             return false;
@@ -13,9 +11,7 @@ $(function () {
 
         let i = 0;
         let inteval = setInterval(function () {
-            $.post(link, { "delay": delay, "content": content, "username": username, "avatar_url": avatar, });
-            time.sleep(delay)
-
+            $.post(link, { "content": content, "username": username, "avatar_url": avatar, });
     
       
         }, 50)
